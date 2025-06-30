f399968a8086ee112716cce9a49eb4562c3f928b
akendewamunyima <wamunyimaakende@gmail.com>
2025-06-30 16:19:46 +0200

    Added Flutter Text

--- lib/flutter_text.dart
+++ lib/flutter_text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 
 void main() {
   // runapp method
@@ -34,7 +35,45 @@ class _MosheHomePageState extends State<MosheHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Moshe Application")),
-      body: Text("Flutter Text"),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            height: 100,
+            child: Text(
+              "Welcome to Moshe's App! Lojng hsgsh jsjsb  xjxshv xsjxhjs uhgs lahsg jxgxyh hxbxwhyw gahxhwxvhn jxxhxngxw",
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          RichText(
+            text: TextSpan(
+              text: "Dont have an account? ",
+              style: TextStyle(fontSize: 20, color: Colors.black),
+              children: [
+                WidgetSpan(
+                  child: Icon(Icons.account_circle, color: Colors.blue),
+                ),
+                TextSpan(
+                  text: " Sign Up",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      // Handle sign up tap
+                      print("Signed Up Tapped");
+                    },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
